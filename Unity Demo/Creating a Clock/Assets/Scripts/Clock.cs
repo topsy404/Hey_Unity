76fa16897c0103e838b5079201e45d76f8fbce6a
topsy <yiliuecust@163.com>
2021-11-08 23:24:43 +0800

    create clock

---
+++ Unity Demo/Creating a Clock/Assets/Scripts/Clock.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Clock : MonoBehaviour
+{
+    [SerializeField]
+    Transform hoursPivot = default, minutesPivot, secondsPivot;
+
+    const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
+
+    void Awake(){
+        DateTime time = DateTime.Now;
+        Debug.Log(time.Hour);
+        Debug.Log(time.Minute);
+        Debug.Log(time.Second);
+        Debug.Log(DateTime.Now.TimeOfDay);
+
+        
+        hoursPivot.localRotation = Quaternion.Euler(0, 0, hoursToDegrees * time.Hour);
+        minutesPivot.localRotation = Quaternion.Euler(0, 0, minutesToDegrees * time.Minute);
+        secondsPivot.localRotation = Quaternion.Euler(0, 0, secondsToDegrees * time.Second);
+
+
+    }
+
+
+    void Update()
+    {
+
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hoursToDegrees * (float)time.TotalHours);
+        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegrees * (float)time.TotalMinutes);
+        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, secondsToDegrees * (float)time.TotalSeconds);
+
+
+    }
+}
+
---
+++ Unity Demo/Creating a Clock/readme.md
@@ -0,0 +1,7 @@
+
+Create clock in Unity
+Unity version: 2019.4
+Author: topsy
+
+
+[ref](https://catlikecoding.com/unity/tutorials/basics/game-objects-and-scripts/)
